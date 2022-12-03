4866b672f33233bda5973c721aee4be41c20b316
doug <byfar357@gmail.com>
2022-12-03 16:30:45 -0500

    search function

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let exArray = [];
 
 //exercise card factory function
-function createExCard(img, exercise, category, summary ){
+function createExCard(img, exercise, category, summary, card){
     return{
-        img, exercise, category, summary,
+        img, exercise, category, summary, card
     }
 };
 
+function createElement(el, cls){
+    let element = document.createElement(el);
+    element.classList.add(cls)
+    return element;
+}
 
-let digiFlex = createExCard("Images/digiflex.jpg", "DigiFlex", "hand", "Hold arm by side with elbow @ 90 degree angle");
+//library of exercise cards
+let digiFlex = createExCard("Images/digiflex.jpg", "DigiFlex", "hand", "Hold arm by side with elbow @ 90 degree angle", createElement("div", "card"));
 add(digiFlex);
-let theraPutty = createExCard("Images/putty.jpg", "TheraPutty", "hand", "");
+let theraPutty = createExCard("Images/putty.jpg", "TheraPutty", "hand", "", createElement("div", "card"));
 add(theraPutty);
-let xTrainer = createExCard("Images/xtrainer.jpg", "X-Trainer", "hand", "")
+let xTrainer = createExCard("Images/xtrainer.jpg", "X-Trainer", "hand", "", createElement("div", "card"))
 add(xTrainer);
-let xtwist = createExCard("Images/xtwist.jpg", "X-Twist", "hand", "");
+let xtwist = createExCard("Images/xtwist.jpg", "X-Twist", "hand", "", createElement("div", "card"));
 add(xtwist);
 
 //push to array function
@@ -25,20 +31,50 @@ function add(name){
 console.log(exArray);
 
 //function to create and render exercise cards
-function displayExerciseCards(){
+function displayExerciseCards(array){
     const container = document.querySelector(".exercises");
-        exArray.forEach(function(item){
-            let div = document.createElement("div");
+        array.forEach(function(item){
             let title = document.createElement("h2");
             title.innerText = item.exercise;
             let image = document.createElement("img");
             image.src = item.img;
             let summary = document.createElement("p");
             summary.innerText = item.summary;
-            div.appendChild(title);
-            div.appendChild(image);
-            container.appendChild(div);
+            summary.classList.add("summary");
+            let sumTab = document.createElement("a");
+            sumTab.innerText = "Summary..."
+            sumTab.classList.add("sum-tab");
+            sumTab.addEventListener("click", function(){
+                summary.classList.toggle("on");
+            });
 
+            item.card.appendChild(title);
+            item.card.appendChild(image);
+            item.card.appendChild(sumTab);
+            item.card.appendChild(summary);
+            container.appendChild(item.card);
+            
+            
         })
+
 }
-displayExerciseCards();
+displayExerciseCards(exArray);
+
+//search functionality
+const searchInput = document.querySelector("#search");
+searchInput.addEventListener("input", () => {
+        let value = searchInput.value.toLowerCase();
+        exArray.forEach(function(item){
+        const visable = item.exercise.toLowerCase().includes(value);
+        if (value == "" || value == " "){
+            item.card.classList.remove("hide");
+        }
+        else if(!visable && value !=""){
+            item.card.classList.add("hide");
+        }
+        else if(visable && value !="")
+            item.card.classList.remove("hide");
+        })
+    }   
+
+        )
